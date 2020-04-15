970424c09b1db2d46ab2a8684ab5e55b5fdf0a15
Tobias Wolff <woltob@Mposteo.de>
2020-04-15 18:06:30 +0200

    Complete React 3.41

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import './card.styles.css'
 
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
 export const Card = (props) => (
     <div className='card-container'>
         <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
         <h2>{props.monster.employee_name}</h2>
-        <p>{props.monster.employee_salary}</p>
+        <p>Salary: {formatter.format(props.monster.employee_salary)}</p>
+        <p>Age: {props.monster.employee_age}</p>
     </div>
 )
